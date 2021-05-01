789cdd9fd8898de8b6f6d882f797612e08f14dbb
mounia fattouh <mounia.fattouh@gmail.com>
2021-05-01 12:16:54 +0200

    Vérifier Nom ,  Prénom , Email : OK

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("form");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,11 +22,87 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// Fermer le modal 
+// Fermer la modal 
 const btnClose = document.querySelector(".close");
 
 btnClose.addEventListener("click", function(){
     modalbg.style.display = "none";
 });
 
+// Ecouter l'évenement Submit
+form.addEventListener("submit" , function(e){
+  
+  if (validPrenom() && validNom() && validEmail()){
+    form.submit();
+  } else {
+    e.preventDefault();
+    alert("les champs ne sont valides")
+  }
+  
+});   
 
+// Vérifier si l'input à au minimum 2 caractères
+const isTwoCharacters = function(input){
+
+  if ((input.value.length) >= 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Vérifier si c'est un email .
+const isEmail = function(input){
+
+  var emailRegExp = new RegExp(
+    '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g'
+    );
+  if (emailRegExp.test(input.value)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+// Vérifier le champ prénom
+
+const validPrenom = function(){
+
+  if (isTwoCharacters(form.first)){
+    form.first.parentNode.classList.remove("error");
+    return true;
+  } else {
+    form.first.parentNode.classList.add("error");
+    return false;
+  }
+
+};
+
+// Vérifier le champ nom
+
+const validNom = function(){
+
+  if (isTwoCharacters(form.last)){
+    form.last.parentNode.classList.remove("error");
+    return true;
+  } else {
+    form.last.parentNode.classList.add("error");
+    return false;
+  }
+
+};
+
+// Vérifier le champ Email 
+
+const validEmail = function(){
+
+  if (isEmail(form.email)){
+    form.email.parentNode.classList.remove("error");
+    return true;
+  } else {
+    form.email.parentNode.classList.add("error");
+    return false;
+  }
+
+};
